fe71f2685f7b909976f3ee8925cb88e5008dca83
Kyle Rosen <kyler1295@gmail.com>
2020-11-25 16:14:05 -0500

    Added Animations and Audience Switcher

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,12 +1,13 @@
+// Outcomes Javascript
 // Global Variables
 const outcomeItems = document.querySelectorAll('.outcomes__item')
 const prevArrow = document.querySelector('.prev-arrow')
 const nextArrow = document.querySelector('.next-arrow')
 
-// Outcomes Code
 let currentOutcome = 0;
 
 const switchOutcomes = (change) => {
+  let tempOutcome = currentOutcome
   if (currentOutcome + change < 0) {
     currentOutcome = outcomeItems.length - 1; 
   } else if (currentOutcome + change > outcomeItems.length - 1) {
@@ -15,9 +16,44 @@ const switchOutcomes = (change) => {
     currentOutcome = currentOutcome + change
   }
   outcomeItems.forEach((outcome, index) => {
-    index === currentOutcome ? outcome.classList.remove('hidden') : outcome.classList.add('hidden')
+    if (index === currentOutcome) {
+      outcome.classList.remove('hidden')
+      outcome.classList.remove('outcome-fadeOut')
+      outcome.classList.add('outcome-fadeIn')
+    } 
+    if (index === tempOutcome) {
+      outcome.classList.remove('outcome-fadeIn')
+      outcome.classList.add('outcome-fadeOut')
+      outcome.classList.add('hidden')
+    }
   });
 }
 
 prevArrow.addEventListener('click', () => switchOutcomes(-1))
 nextArrow.addEventListener('click', () => switchOutcomes(1))
+
+
+
+
+
+// Audience Javascript
+const select = document.querySelector('select')
+const audienceSections = document.querySelectorAll('.audience-items__subset')
+
+const switchAudience = (audience) => {
+  audienceSections.forEach((audienceSection) => {
+    let dataAudience = audienceSection.getAttribute('data-audience')
+    if (!audienceSection.classList.contains('hidden')) {
+      audienceSection.classList.remove('audience-fadeIn')
+      audienceSection.classList.add('audience-fadeOut')
+      audienceSection.classList.add('hidden')
+      setTimeout(audienceSection.classList.remove('audience-fadeOut'), 1000)
+    }
+    if (dataAudience === audience) {
+      audienceSection.classList.remove('hidden')
+      audienceSection.classList.add('audience-fadeIn')
+    }
+  })
+}
+
+select.addEventListener('change', () => switchAudience(select.value))
